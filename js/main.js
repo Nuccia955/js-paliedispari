0eaf369cd624cb0eb67ee8750bd1466f4cc1a083
Nuccia955 <elenamunaro1995@gmail.com>
2021-10-29 14:30:35 +0200

    add sketch logic for palindrome task

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*PALINDROME WORD TASK  */
+//2. ask user a word and store in a variable
+//3. call function for checking user input
+//4. if user input is palindrome print out "the word is palindrome"
+//   else "the word isn't palindrome"
+
+//1. define a function for checking if user input is palindrome or not
