43a1c40824e1a32b8e4029ce3bb7ad777633f4d8
Nuccia955 <elenamunaro1995@gmail.com>
2021-10-29 15:16:13 +0200

    complete palindrome word task logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 /*PALINDROME WORD TASK  */
 //2. ask user a word and store in a variable
-//3. call function for checking user input
-//4. if user input is palindrome print out "the word is palindrome"
-//   else "the word isn't palindrome"
+const userWord = prompt('Type a word');
+console.log(userWord);
 
+//3. call function for checking user input
+const result = checkForPalindrome(userWord);
+console.log(result);
+
+
+/**********************
+ * FUNCTION PALINDROME
+ **********************/
 //1. define a function for checking if user input is palindrome or not
+/**
+ * 
+ * @param {string} word 
+ * @returns
+ */
+function checkForPalindrome (word) {
+    //1.1. word reverse
+    let userReverse = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        userReverse += word[i];
+    }
+    console.log(userReverse);
+    //1.2. if word is palindrome print out "the word is palindrome"
+    //     else "the word isn't palindrome"
+    if (userReverse === word) {
+        return 'The word you chose is palindrome';
+    } else {
+        return 'The word is not palindrome';
+    }
+}
