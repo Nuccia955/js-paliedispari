b70fdb5e564e62e71e72a6f523570a200a0c2d97
Nuccia955 <elenamunaro1995@gmail.com>
2021-10-30 02:07:23 +0200

    add tasks logic on html

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 /*PALINDROME WORD TASK  */
-//2. ask user a word and store in a variable
-/* const userWord = prompt('Type a word');
-console.log(userWord);
-
-//3. call function for checking user input
-const result = checkForPalindrome(userWord);
-console.log(result); */
+//2. add a click event on button "Converti"
+const btnWord = document.getElementById('btn-word');
+btnWord.addEventListener('click', function() {
+    //3. ask user a word and store in a variable
+    let userWord = prompt('choose a word!');
+    while (!isNaN(parseInt(userWord))) {
+        alert ('Please choose a word, not a nomber');
+        userWord = prompt('choose a word!');
+    }
+    let userWordChoice = document.querySelector('h1');
+    userWordChoice.innerHTML += `<span>You chose: ${userWord}</span>`;
+    let wordResult = document.getElementById('word-result');
+    //4. call function for checking user input
+    const result = checkForPalindrome(userWord);
+    wordResult.innerHTML += `${result}. Refresh the page to try again!`;
+})
 
 
 /**********************
@@ -17,54 +26,64 @@ console.log(result); */
  * @param {string} word 
  * @returns
  */
-/* function checkForPalindrome (word) {
+function checkForPalindrome (word) {
     //1.1. word reverse
     let userReverse = '';
     for (let i = word.length - 1; i >= 0; i--) {
         userReverse += word[i];
     }
-    console.log(userReverse);
     //1.2. if word is palindrome print out "the word is palindrome"
     //     else "the word isn't palindrome"
     if (userReverse === word) {
-        return 'The word you chose is palindrome';
+        return `Reverse ${userReverse}: the word you chose is palindrome`;
     } else {
-        return 'The word is not palindrome';
+        return `Reverse ${userReverse}: the word is not palindrome`;
     }
-} */
+}
 
 
 
 /* ODD/EVEN TASK */
 //2. ask user if odd or even
-let userChoice = prompt('odd or even?');
-while (userChoice !== 'odd' && userChoice !== 'even') {
-    alert('Please type "odd" or "even"');
-    userChoice = prompt('odd or even?');
-}
-console.log(`You chose ${userChoice}`);
-if (userChoice === 'even') {
-    userChoice = true;
-} else {
-    userChoice = false;
-}
-//3. ask user a number from 1 to 5
-const userNumber = parseInt(prompt('Type a number from 1 to 5'));
-console.log(`You chose ${userNumber}`)
-//4. call function for generating a random number from 1 to 5
-const computerNumber = genRandomNumber(5, 1);
-console.log(`The computer choice is ${computerNumber}`);
-//5. sum the numbers
-const sum = userNumber + computerNumber;
-console.log(`The sum is ${sum}`)
-//7. call function for checking if odd or even
-const sumResult = checkOddOrEven(sum);
-//8. get the winner
-if(userChoice === sumResult) {
-    console.log('You won');
-} else {
-    console.log('You lose');
-}
+const btnOddEven = document.getElementById('btn-odd-even');
+btnOddEven.addEventListener('click', function() {
+    let userChoice = prompt('odd or even?');
+    while (userChoice !== 'odd' && userChoice !== 'even') {
+        alert('Please type "odd" or "even"');
+        userChoice = prompt('odd or even?');
+    }
+    const oddEven = document.querySelector('.odd-even > h1');
+    oddEven.innerHTML += ` You chose ${userChoice}`;
+    if (userChoice === 'even') {
+        userChoice = true;
+    } else {
+        userChoice = false;
+    }
+    //3. ask user a number from 1 to 5
+    let userNumber = parseInt(prompt('Type a number from 1 to 5'));
+    while (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
+        alert('You have to choose a number from 1 to 5')
+        userNumber = parseInt(prompt('Type a number from 1 to 5'));
+    }
+    oddEven.innerHTML += ` and you chose ${userNumber}`;
+    //4. call function for generating a random number from 1 to 5
+    const computerNumber = genRandomNumber(5, 1);
+    const computerChoice = document.querySelector('.odd-even > h3');
+    computerChoice.innerHTML = `The computer choice is ${computerNumber}.`;
+    //5. sum the numbers
+    const sum = userNumber + computerNumber;
+    computerChoice.innerHTML += ` The sum of the numbers is ${sum}`;
+    //7. call function for checking if odd or even
+    const sumResult = checkOddOrEven(sum);
+    //8. get the winner
+    const result = document.getElementById('result');
+    if(userChoice === sumResult) {
+        result.innerHTML = 'You won!';
+    } else {
+        result.innerHTML = 'You lose';
+    }
+})
+
 
 
 
@@ -84,13 +103,15 @@ function genRandomNumber(max, min) {
 }
 
 //6. define a function for checking if odd or even
+/**
+ * 
+ * @param {number} number 
+ * @returns 
+ */
 function checkOddOrEven(number) {
     let even = true;
     if (number % 2 !== 0) {
         even = false;
-        console.log('The result is odd');
-    } else {
-        console.log('The result is even');
     }
     return even;
 }
